43e736e059e7be33daebe39360e3e1fbdfe54e0c
via <847267507@qq.com>
2020-07-18 18:14:56 +0800

    fixed:score-bug

--- cyan-mango/pages/Campus/score/index.js
+++ cyan-mango/pages/Campus/score/index.js
@@ -1,105 +1,114 @@
-import { GradeModel } from './score'
-import { config } from '../../../utils/config'
+import {
+  GradeModel
+} from './score'
+import {
+  config
+} from '../../../utils/config'
 // var Data = require("../../../../utils/setting/data")
 const app = getApp()
 var grade = new GradeModel()
 let height = 360
-Page({
 
+Page({
   data: {
-    showDrawer:false,
-    loading:true,
-    hasGrade:false,
+    modal_title: '',
+    grade_index: '',
+    showDrawer: false,
+    loading: false,
+    hasGrade: false,
     hideSyncTip: true,
     refleshTimes: 0,
     showTips: false,
-    current_term:true,
-    current:0,
-    showDetail:false,
-    grade:'',
-    sem_list:[],
-    color:["gradual-red", "gradual-orange", "gradual-green", "gradual-blue", "gradual-purple", "gradual-pink", "gradual-d","gradual-c"],
-    flagColor: ["#DCDCDC", "#FA8072", "#FFFBE5", "#D8BFD8", "#AFEEEE","#98FB98"]
+    current_term: true,
+    current: 0,
+    showDetail: false,
+    showModal: false,
+    grade: '',
+    sem_list: [],
+    bg_color: ['bg-grey', 'bg-brown', 'bg-yellow', 'bg-mauve', 'bg-cyan', 'bg-green'],
+    color: ["gradual-red", "gradual-orange", "gradual-green", "gradual-blue", "gradual-purple", "gradual-pink", "gradual-d", "gradual-c"],
+    // flagColor: ["#f43f3b", "#FA8072", "#FFFBE5", "#D8BFD8", "#AFEEEE", "#AFEEEE"]
   },
-  confirm(){
+  confirm() {
     this.putExamHistory()
     this.setData({
-      showDrawer:true
+      showDrawer: true
     })
   },
-  showDetail(e){
-    let index=e.currentTarget.id
+  showDetail(e) {
+    let index = e.currentTarget.id
     this.setData({
-      showDetail:true,
-      detail:this.data.grade.score[index]
+      showDetail: true,
+      detail: this.data.grade.score[index]
 
     })
   },
-  showItem(){
+  showModal(e) {
     this.setData({
-      showDrawer:!this.data.showDrawer
+      index: this.data.grade_index,
+      showModal: true,
+      grade: this.data.grade,
     })
-    this.putExamHistory()
   },
-  cancelModal(){
-    
+  showItem() {
+    this.setData({
+      showDrawer: true
+    })
+    // this.putExamHistory()
+  },
+  cancelModal() {
+
   },
   navBack() {
     wx.navigateBack()
   },
-  async getGradeDatail(e){
-    let str= e.currentTarget.dataset.xnd.split("-")
-    let data={
-      start:str[0],
-      end:str[1],
-      term:e.currentTarget.dataset.xqd
+  async getGradeDatail(e) {
+
+    let str = e.currentTarget.dataset.xnd.split("-")
+    let data = {
+      start: str[0],
+      end: str[1],
+      term: e.currentTarget.dataset.xqd
     }
-    let res=await grade.postGrade(data)
-    console.log('成绩',res)
+    let res = await grade.postGrade(data)
+
     this.setData({
-      grade:this.score_sort(res.data),
-      hasGrade:true,
-      loading:false,
-      showDrawer:false
+      grade_index: e.currentTarget.id,
+      grade: this.score_sort(res.data),
+      hasGrade: true,
+      loading: false,
+      showDrawer: false
     })
   },
-  async updateGrade(){
-    let res=await grade.score()
-    console.log(res)
-    if(res.error_code==1){
+  async updateGrade() {
+    // 获取本学期的成绩信息
+    this.setData({
+      loading: true,
+    })
+    let res = await grade.score()
+    if (res.error_code == 0) {
       this.setData({
-        current_term:false,
-        loading:false
+        grade: this.score_sort(res.data),
+        loading: false,
+        hasGrade: true,
       })
-    }else if(res.error_code==5010){
+    } else if (res.error_code == 1) {
       this.setData({
+        current_term: false,
+        modal_title: '暂时还未到本学期成绩',
         loading: false
       })
-      wx.showToast({
-        title: res.msg,
-        icon: 'none',
-        duration: 2500,
-        mask: true
-      })
-    }else if(res.error_code==0){
-      console.log('本学期成绩',res.data)
-      this.setData({
-        grade:this.score_sort(res.data),
-        loading:false,
-        hasGrade:true,
-      })
-    }
-    else if(res.error_code == 4003){
+    } else if (res.error_code == 4003) {
       /* 权限校验失败 */
       this.setData({
         loading: false
       })
       wx.showModal({
         title: '提示',
-        content:  res.msg,
-        confirmText:'去绑定',
-        cancelText:'返回',
-        success (res) {
+        content: res.msg,
+        confirmText: '去绑定',
+        cancelText: '返回',
+        success(res) {
           if (res.confirm) {
             wx.navigateTo({
               url: '/pages/Setting/login/index',
@@ -111,43 +120,65 @@ Page({
           }
         }
       })
-    }
-    else{
+    } else {
+      let msg = '';
+      if(res.error_code == 5250) msg = '暂时无法查询学期成绩';
+      else msg = res.msg;
       this.setData({
-        loading:false
-      })      
+        loading: false,
+        current_term: false,
+        modal_title: msg
+      })
+
+      
     }
   },
   // 更新数据库
-  async putExamHistory(){
+  async putExamHistory() {
+    // 所有学期的数据
     this.setData({
-      loading:true
+      loading: true
     })
-    let res = await grade.putExamHistory()
-    console.log(res)
-    if(res.error_code==0){
-      let semester=await grade.semester()
-      console.log(semester)
+    var res = await grade.getExamHistory()
+    if (res.error_code) {
+      var res = await grade.putExamHistory()
+    }
+    if (res.error_code == 0) {
+      let semester = await grade.semester()
+      
       this.setData({
-        loading:false,
-        sem_list:semester.data
+        loading: false,
+        sem_list: semester.data.sort((a,b)=>{if(a.xnd==b.xnd) return b.xqd - a.xqd})
       })
-      wx.setStorageSync('semester', semester.data)
-    }else{
+      // wx.setStorageSync('semester', semester.data)
+    } else {
       this.setData({
-        loading:false
+        loading: false
+      })
+      wx.showModal({
+        title: '提示',
+        content: res.msg,
+        cancelText: '返回',
+        success(res) {
+          
+            wx.navigateBack({
+              complete: (res) => {},
+            })
+          
+        }
+
       })
     }
   },
   score_sort(data) {
     let score = data.score;
-    for(var i=0; i < score.length; i++){
+    for (var i = 0; i < score.length; i++) {
       let min = i;
-      
-      for(var j = i+1; j < score.length; j++){
+
+      for (var j = i + 1; j < score.length; j++) {
         let n = Number(score[min].grade);
         let m = Number(score[j].grade)
-        if(isNaN(n)){
+        if (isNaN(n)) {
           if (score[min].grade == '不及格') n = 50;
           else if (score[min].grade == '及格') n = 65;
           else if (score[min].grade == '中等') n = 75;
@@ -155,7 +186,7 @@ Page({
           else if (score[min].grade == '优秀') n = 95;
           else n = 0;
         }
-        if(isNaN(m)){
+        if (isNaN(m)) {
           if (score[j].grade == '不及格') m = 50;
           else if (score[j].grade == '及格') m = 65;
           else if (score[j].grade == '中等') m = 75;
@@ -163,7 +194,7 @@ Page({
           else if (score[j].grade == '优秀') m = 95;
           else m = 0;
         }
-        if ( n > m) {
+        if (n > m) {
           min = j;
         }
 
@@ -177,9 +208,12 @@ Page({
   onLoad: function (options) {
 
   },
-onHide(){
+  onHide() {
+    this.setData({
+      loading: false
+    })
 
-},
+  },
   onShow: function () {
     var time = new Date()
     if (time.getHours() >= 0 && time.getHours() < 7) {
@@ -187,7 +221,6 @@ onHide(){
         hideSyncTip: false
       })
     }
-    
     this.updateGrade()
   },
 
